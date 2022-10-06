6b6285d2a3a823b412edf028e8a38a96b32549a0
Diego Fernandes <diego.schell.f@gmail.com>
2022-10-06 10:07:19 -0300

    ci: Add TurboRepo cache on deploy build

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -7,6 +7,31 @@ export default {
   component: Button,
   args: {
     children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
+  },
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    size: {
+      options: ['sm', 'md'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    onClick: {
+      action: 'click',
+    },
   },
 } as Meta<ButtonProps>
 
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @ignite-ui/react
+
+## 2.0.0
+
+### Major Changes
+
+- Add display name to all components
